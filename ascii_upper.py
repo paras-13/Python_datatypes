14009c6797e19d41a7e2e8b9d2d223ead2fc9928
Paras Upadhyay <paras13upadhyay@gmail.com>
2022-11-21 13:26:19 +0530

    Capitalize using ASCII value

---
+++ ascii_upper.py
@@ -0,0 +1,15 @@
+# Program to make the first letter of each word in the sentence capita using ASCII value.
+
+Str = input("Enter a string")
+lst = Str.split(" ")
+Str = ""
+for i in lst:
+    alpha,remain= i[0],i[1:]
+    Ascii = ord(alpha)
+    if Ascii > 90:
+        alpha = chr(Ascii-32)
+        remain = alpha+remain+" "
+        Str+=remain
+    else:
+        Str+= i+" "
+print(Str)
